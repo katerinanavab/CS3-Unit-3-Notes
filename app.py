23d2af703febdbf076d65e71fcdf5b7c501ef103
Katerina Navab <94501766+katerinanavab@users.noreply.github.com>
2025-01-16 19:46:47 +0000

    fixed route error

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def index():
 
 
 @app.route("/hello/")
-@app.route("/hello/<name>")
+@app.route("/hello/<name_data>")
 def hello_there(name_data = None):
     return render_template("hello_there.html", name=name_data)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

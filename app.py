950ce470ec22511ce5d176c30600a161b0c79afc
Katerina Navab <94501766+katerinanavab@users.noreply.github.com>
2025-01-16 19:28:51 +0000

    added templates

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -7,6 +8,8 @@ app = Flask(__name__)
 def index():
     return "Hello, world!"
 
-if __name__ == "__main__": 
-    app.run(host='0.0.0.0', port=9874)
-    
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name_data = None):
+    return render_template("hello_there.html", name=name_data)

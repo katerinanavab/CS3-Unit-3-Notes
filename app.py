d0a0d50256b39eeff1d2be899b0410cdc85f8fd9
Katerina Navab <94501766+katerinanavab@users.noreply.github.com>
2025-01-17 16:11:46 +0000

    fixed everything.

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 # Index route for web app
 @app.route("/")
 def index():
-    return "Hello, world!"
+    # return "Hello, world!"
+    return render_template("index.html")
 
 
 @app.route("/hello/")
@@ -14,6 +15,6 @@ def index():
 def hello_there(name_data = None):
     return render_template("hello_there.html", name=name_data)
 
-# Allows you to click "Run" button
+# Allows you to click RUN button
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5421)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,19 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+# Index route for web app
+@app.route("/")
+def index():
+    return "Hello, world!"
+
+
+@app.route("/hello/")
 @app.route("/hello/<name_data>")
+def hello_there(name_data = None):
+    return render_template("hello_there.html", name=name_data)
+
+# Allows you to click "Run" button
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

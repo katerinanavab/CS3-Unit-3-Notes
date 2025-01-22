27847103cea529517bcc5836785ea6cf7fb917bb
Katerina Navab <94501766+katerinanavab@users.noreply.github.com>
2025-01-22 15:41:45 +0000

    added template inheritance

--- app.py
+++ app.py
@@ -3,16 +3,31 @@ from flask import render_template
 
 app = Flask(__name__)
 
-# Index route for web app
+# Index (homepage) route for web app
 @app.route("/")
 def index():
     # return "Hello, world!"
     return render_template("index.html")
 
+ # New functions
+@app.route("/about/")
+def about():
+     return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+     return render_template("contact.html")
+
 @app.route("/hello/")
 @app.route("/hello/<name_data>")
 def hello_there(name_data = None):
     return render_template("hello_there.html", name=name_data)
+
+@app.route("/api/data")
+def get_data():
+  # displays data file returned from an API call
+  # can do more Python work to format this later
+  return app.send_static_file("data.json")  
 
 # Allows you to click RUN button
 if __name__ == "__main__":
